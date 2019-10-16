e7c2c8c1c2ace1a51741d9fc58f84de9fafd3bbc
Jonathan Jopio <jonathanjopio@gmail.com>
2019-10-16 01:38:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+
+participants = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(participants)
+  participants_badge = []
+  participants.each do |participants|
+    participants_badge << badge_maker(participants)
+  end
+end
+
+def assign_rooms
+  
+end
+
+def printer
+  
+end
