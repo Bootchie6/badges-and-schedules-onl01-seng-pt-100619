eca4fe8e46074dd4ed18a67aebe76a626cb8198f
Jonathan Jopio <jonathanjopio@gmail.com>
2019-10-16 01:44:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,14 +8,23 @@ end
 
 def batch_badge_creator(participants)
   participants_badge = []
+ 
   participants.each do |participants|
     participants_badge << badge_maker(participants)
+ 
   end
   participants_badge
 end
 
 def assign_rooms
+  participants_room = []
+  room = 1
   
+  while room < 8 do
+    participants.each do |participants|
+      participants_room << "Hello, #{participants}! You'll"
+    end
+  end
 end
 
 def printer
